e214f4d4607a6c321165ca6a0965652889ba3afe
Andrey Semenov <semenov.orc@gmail.com>
2024-05-10 00:13:38 +0300

    server response done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
 import setTitle from './setTitle'
 
 setTitle('HelpDesk')
+
+const fetchData = async () => {
+  return await fetch('http://localhost:3000/test/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: 'John', age: 30 }),
+  })
+    .then((response) => response.json())
+    .then((data) => logResponse(data))
+}
+
+const logResponse = (response) => {
+  console.log(response)
+}
+
+fetchData()
